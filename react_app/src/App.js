4774dd241a1ac6615cd3a7a25b388fd036165204
subeg7 <subeg7@gmail.com>
2021-02-10 13:23:21 +0545

    render pdf in react app

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PDFViewer from 'pdf-viewer-reactjs'
+
+// const ExamplePDFViewer = () => {
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { isPdfUrlRecieved: false, pdfUrl: "", count: 0 };
+  }
+
+
+
+  setUrl = (result) => {
+    console.log("inside setUrl method in javascript");
+    this.setState({ isPdfUrlRecieved: true, pdfUrl: result });
+  }
+
+  render() {
+    return (
+      this.state.isPdfUrlRecieved ?
+        <PDFViewer
+          document={{
+            // url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+            // url: 'http://www.africau.edu/images/default/sample.pdf'
+            // url: 'https://cors-anywhere.herokuapp.com/http://www.africau.edu/images/default/sample.pdf'
+            // url: 'https://cors-anywhere.herokuapp.com/https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf',
+            url: this.state.pdfUrl,
+          }}
+        />
+        : <div onClick={() => this.setUrl('https://cors-anywhere.herokuapp.com/http://www.africau.edu/images/default/sample.pdf')}>
+          <h1>Waiting for flutter to send pdf link [{this.state.count}]</h1>
+        </div>
+    )
+  }
 }
-
-export default App;
