bedce37419b4f973bbe53036ecc657843d540aa7
subeg7 <subeg7@gmail.com>
2021-02-13 13:17:00 +0545

    Update app.js with sample pdf details

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,19 +2,23 @@ import React from 'react'
 import PDFViewer from 'pdf-viewer-reactjs'
 
 const herokuCorsProtectionUrl = "https://cors-anywhere.herokuapp.com/";
+const samplePdfDetails = {
+  "fileName": "Flutter PDF article",
+  "fileLink": "https://ijesc.org/upload/b3930ac14331fd1b425af8cd1c341d41.Cross%20Platform%20Development%20using%20Flutter%20(1).pdf",
+}
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       hasEventOccurred: false,
+      fileName: "",
       fileLink: "",
-      fileName: ""
     };
   }
 
   handleEvent = (name, link) => {
-    console.log("inside setData method in javascript");
+    console.log("inside the handleEvent method");
     this.setState({ hasEventOccurred: true, fileLink: link, fileName: name });
   }
 
@@ -30,7 +34,7 @@ export default class App extends React.Component {
           />
         </div>
 
-        : <div onClick={() => this.handleEvent("African PDF", herokuCorsProtectionUrl + 'https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf?hsLang=en')}>
+        : <div onClick={() => this.handleEvent(samplePdfDetails.fileName, herokuCorsProtectionUrl + samplePdfDetails.fileLink)}>
           < h2> No event has occurred yet</h2 >
           < h3>click to see sample change</h3 >
         </div >
