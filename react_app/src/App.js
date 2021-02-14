5b3f714ea3a051aab2dd1d8ec99f270cea737c03
subeg7 <subeg7@gmail.com>
2021-02-14 23:34:44 +0545

    [React App] update handle event method

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PDFViewer from 'pdf-viewer-reactjs'
 
-const herokuCorsProtectionUrl = "https://cors-anywhere.herokuapp.com/";
 const samplePdfDetails = {
   "fileName": "Flutter PDF article",
   "fileLink": "https://ijesc.org/upload/b3930ac14331fd1b425af8cd1c341d41.Cross%20Platform%20Development%20using%20Flutter%20(1).pdf",
@@ -27,8 +26,9 @@ export default class App extends React.Component {
   }
 
   handleEvent = (name, link) => {
-    console.log("inside the handleEvent method");
-    this.setState({ hasEventOccurred: true, fileLink: link, fileName: name });
+    const herokuCorsProtectionUrl = "https://cors-anywhere.herokuapp.com/";
+    console.log("Event has occurred, rendering pdf now");
+    this.setState({ hasEventOccurred: true, fileLink: herokuCorsProtectionUrl + link, fileName: name });
   }
 
   render() {
@@ -43,7 +43,7 @@ export default class App extends React.Component {
           />
         </div>
 
-        : <div onClick={() => this.handleEvent(samplePdfDetails.fileName, herokuCorsProtectionUrl + samplePdfDetails.fileLink)}>
+        : <div onClick={() => this.handleEvent(samplePdfDetails.fileName, samplePdfDetails.fileLink)}>
           < h2> No event has occurred yet</h2 >
           < h3>click to see sample change</h3 >
         </div >
